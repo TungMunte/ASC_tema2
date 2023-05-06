3287d92e30bca012c5cf16f947bb205ee0aae5dc
Tung NGUYEN QUANG 322CD <seeyouagain14012000@gmail.com>
2023-05-06 09:23:48 +0300

    second commit

--- solver_neopt.c
+++ solver_neopt.c
@@ -7,15 +7,13 @@
 /*
  * Add your unoptimized implementation here
  */
-void allocate_matrices(int N, double **AB, double **ABA_t, double **C, double **D, double **A_t, double **B_t)
+void allocate_matrices(int N, double **AB, double **ABA_t, double **C, double **D, double **B_t)
 {
 	*AB = calloc(N * N, sizeof(**AB));
 	*ABA_t = calloc(N * N, sizeof(**ABA_t));
 	*C = calloc(N * N, sizeof(**C));
 	*D = calloc(N * N, sizeof(**D));
-
-	*A_t = malloc(N * N * sizeof(**A_t));
-	*B_t = malloc(N * N * sizeof(**B_t));
+	*B_t = calloc(N * N, sizeof(**B_t));
 }
 
 double *my_solver(int N, double *A, double *B)
@@ -24,20 +22,17 @@ double *my_solver(int N, double *A, double *B)
 	double *ABA_t; // for A * B * A_t
 	double *C;	   // for B_t * B_t
 	double *D;	   // for A * B * A_t + B_t * B_t
+	double *B_t;   // for transposed B
 
-	double *A_t;
-	double *B_t;
-
-	allocate_matrices(N, &AB, &ABA_t, &C, &D, &A_t, &B_t);
+	allocate_matrices(N, &AB, &ABA_t, &C, &D, &B_t);
 
 	/*
-		transpose A, B
+		transpose B
 	*/
 	for (int i = 0; i < N; i++)
 	{
 		for (int j = 0; j < N; j++)
 		{
-			A_t[i * N + j] = A[j * N + i];
 			B_t[i * N + j] = B[j * N + i];
 		}
 	}
@@ -49,7 +44,7 @@ double *my_solver(int N, double *A, double *B)
 	{
 		for (int j = 0; j < N; j++)
 		{
-			for (int k = 0; k < N; k++)
+			for (int k = i; k < N; k++)
 			{
 				AB[i * N + j] += A[i * N + k] * B[k * N + j];
 			}
@@ -63,9 +58,9 @@ double *my_solver(int N, double *A, double *B)
 	{
 		for (int j = 0; j < N; j++)
 		{
-			for (int k = 0; k < N; k++)
+			for (int k = j; k < N; k++)
 			{
-				ABA_t[i * N + j] += AB[i * N + k] * A_t[j * N + k];
+				ABA_t[i * N + j] += AB[i * N + k] * A[j * N + k];
 			}
 		}
 	}
@@ -79,7 +74,7 @@ double *my_solver(int N, double *A, double *B)
 		{
 			for (int k = 0; k < N; k++)
 			{
-				C[i * N + j] += B_t[k * N + i] * B_t[k * N + j];
+				C[i * N + j] += B_t[i * N + k] * B_t[k * N + j];
 			}
 		}
 	}
@@ -95,7 +90,6 @@ double *my_solver(int N, double *A, double *B)
 		}
 	}
 
-	free(A_t);
 	free(B_t);
 	free(AB);
 	free(ABA_t);
--- solver_opt.c
+++ solver_opt.c
@@ -7,8 +7,115 @@
 /*
  * Add your optimized implementation here
  */
+
+void allocate_matrices(int N, double **AB, double **ABA_t, double **C, double **D, double **B_t)
+{
+	register int count = N * N;
+	register int size = sizeof(**AB);
+	*AB = calloc(count, size);
+	*ABA_t = calloc(count, size);
+	*C = calloc(count, size);
+	*D = calloc(count, size);
+	*B_t = calloc(count, size);
+}
+
 double *my_solver(int N, double *A, double *B)
 {
+	double *AB;	   // for A * B
+	double *ABA_t; // for A * B * A_t
+	double *C;	   // for B_t * B_t
+	double *D;	   // for A * B * A_t + B_t * B_t
+	double *B_t;   // for transposed B
 
-	return NULL;
+	allocate_matrices(N, &AB, &ABA_t, &C, &D, &B_t);
+
+	/*
+		transpose B
+	*/
+	for (register int i = 0; i < N; i++)
+	{
+		register double *B_t_ptr = B_t + i;
+		register double *B_ptr = B + i * N;
+		for (register int j = 0; j < N; j++, B_t_ptr += N, B_ptr++)
+		{
+			*B_t_ptr = *B_ptr;
+		}
+	}
+
+	/*
+		calculate A * B
+	*/
+	for (register int i = 0; i < N; i++)
+	{
+		register double *AB_ptr = AB + i * N;
+		register double *A_ptr = A + i * N;
+		for (register int j = 0; j < N; j++, AB_ptr++)
+		{
+			register double result = 0;
+			register double *B_ptr = B + j;
+			for (register int k = i; k < N; k++)
+			{
+				result += *(A_ptr + k) * *(B_ptr + k * N);
+			}
+			*AB_ptr = result;
+		}
+	}
+
+	/*
+		calculate A * B * A_t
+	*/
+	for (register int i = 0; i < N; i++)
+	{
+		register double *ABA_t_ptr = ABA_t + i * N;
+		register double *AB_ptr = AB + i * N;
+		for (register int j = 0; j < N; j++, ABA_t_ptr++)
+		{
+			register double result = 0;
+			register double *A_ptr = A + j * N;
+			for (register int k = j; k < N; k++)
+			{
+				result += *(AB_ptr + k) * *(A_ptr + k);
+			}
+			*ABA_t_ptr = result;
+		}
+	}
+
+	/*
+		calculate B_t * B_t
+	*/
+	for (register int i = 0; i < N; i++)
+	{
+		register double *C_ptr = C + i * N;
+		register double *B_t_ptr_1 = B_t + i * N;
+		for (register int j = 0; j < N; j++, C_ptr++)
+		{
+			register double result = 0;
+			register double *B_t_ptr_2 = B_t + j;
+			for (register int k = 0; k < N; k++)
+			{
+				result += *(B_t_ptr_1 + k) * *(B_t_ptr_2 + k * N);
+			}
+			*C_ptr = result;
+		}
+	}
+
+	/*
+		calculate A * B * A_t + B_t * B_t
+	*/
+	for (register int i = 0; i < N; i++)
+	{
+		register double *C_ptr = C + i * N;
+		register double *ABA_t_ptr = ABA_t + i * N;
+		register double *D_ptr = D + i * N;
+		for (register int j = 0; j < N; j++, C_ptr++, ABA_t_ptr++, D_ptr++)
+		{
+			*D_ptr = *C_ptr + *ABA_t_ptr;
+		}
+	}
+
+	free(B_t);
+	free(AB);
+	free(ABA_t);
+	free(C);
+	return D;
 }
